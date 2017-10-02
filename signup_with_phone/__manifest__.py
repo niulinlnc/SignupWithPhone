0da3c9b38d9e412d8e3819abaa0002d5803c92f8
Khyati_Nishuta <khyati.nishtatechnolabs@gmail.com>
2017-10-02 16:40:47 +0530

    Initial Commit for Sign up with Phone Number in Odoo Version 10.0

---
+++ signup_with_phone/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Signup With Mobile Number',
+    'category': 'Website',
+    'summary': 'Signup With Mobile (If not Email).',
+    'website': '',
+    'version': '1.0',
+    'description': """
+    
+    This module can create users from the Odoo main screen as an external user and signup with the same credentials. 
+    It mainly provides validation of emailid and mobile number during signup. 
+    User id has to be  unique for every account so email id and mobile number cannot be duplicated.
+    
+        """,
+    'depends': ['auth_signup'],
+    'data': [
+        'views/auth_signup_inherit.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
